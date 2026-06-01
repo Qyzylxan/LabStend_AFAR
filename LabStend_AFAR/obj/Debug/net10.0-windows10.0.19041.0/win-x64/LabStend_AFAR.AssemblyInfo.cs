a2603ae0dca5a94d0e5272fb8c72aa27e1074219
Андрей Тан <a.tan@intsys.space>
2026-06-01 20:14:14 +0300

    ToolBars Added, Styles & Colors edited

--- LabStend_AFAR/obj/Debug/net10.0-windows10.0.19041.0/win-x64/LabStend_AFAR.AssemblyInfo.cs
+++ LabStend_AFAR/obj/Debug/net10.0-windows10.0.19041.0/win-x64/LabStend_AFAR.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabStend_AFAR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+34572629760e2287778d8be71917a37c8d8e044c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4b6cf3f4c4aec1c17b66c666a836640b615b3557")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabStend_AFAR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabStend_AFAR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
